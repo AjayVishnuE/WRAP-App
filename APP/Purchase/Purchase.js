1b0b0dd2e1087d929d081201df86a38ad7c9c244
Ajay Vishnu E <ajayvishnu121@gmail.com>
2023-03-29 11:25:02 +0530

    Add files via upload

---
+++ APP/Purchase/Purchase.js
@@ -0,0 +1,45 @@
+// order bin inc dec button
+function decrement(e) {
+    const btn = e.target.parentNode.parentElement.querySelector(
+      'button[data-action="decrement"]'
+    );
+    const target = btn.nextElementSibling;
+    let value = Number(target.value);
+    if (value > 0)
+      {
+        value--;
+        target.value = value;
+      }
+    else{
+      {
+        value=0
+      }
+    }
+    
+  }
+
+  function increment(e) {
+    const btn = e.target.parentNode.parentElement.querySelector(
+      'button[data-action="decrement"]'
+    );
+    const target = btn.nextElementSibling;
+    let value = Number(target.value);
+    value++;
+    target.value = value;
+  }
+
+  const decrementButtons = document.querySelectorAll(
+    `button[data-action="decrement"]`
+  );
+
+  const incrementButtons = document.querySelectorAll(
+    `button[data-action="increment"]`
+  );
+
+  decrementButtons.forEach(btn => {
+    btn.addEventListener("click", decrement);
+  });
+
+  incrementButtons.forEach(btn => {
+    btn.addEventListener("click", increment);
+  });
